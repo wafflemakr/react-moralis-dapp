1de53ce0986a6e494a33a2888de58f94a8d9cfeb
FELIPE <wafflemakr@gmail.com>
2021-05-27 12:49:11 -0500

    initial moralis dapp with chakra ui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { MoralisProvider } from "react-moralis";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      appId={process.env.REACT_APP_MORALIS_ID}
+      serverUrl={process.env.REACT_APP_MORALIS_URL}
+    >
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
